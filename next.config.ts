0ad75846a8b444250e5f13d20707c1aadb31d8c5
DdangJeong <89457765+DdangJeong@users.noreply.github.com>
2025-01-16 15:51:29 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // 이미지 최적화 비활성화
   },  
+  experimental: {
+    serverActions: false, // Server Actions 비활성화
+  },  
 }
 
 export default nextConfig
