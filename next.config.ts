8c779e9d528dc0cb419beb35fc2b9119ce0d0396
DdangJeong <89457765+DdangJeong@users.noreply.github.com>
2025-01-16 15:35:49 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
     EMAIL_USER: process.env.EMAIL_USER,
     EMAIL_APP_PASSWORD: process.env.EMAIL_APP_PASSWORD,
   },
+  output: 'export', // 정적 파일 생성 활성화
+  basePath: '', // '/<repository-name>'로 설정 (필요시)
+  images: {
+    unoptimized: true, // 이미지 최적화 비활성화
+  },  
 }
 
 export default nextConfig
